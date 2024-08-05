ae6ba5976c8097fd2937bf155ddde06946804351
Haider Abbas <haidertgr677@gmail.com>
2024-08-05 20:07:19 +0530

    Calculator App was modified

--- Mini-Projects/Calculator App/calculator.py
+++ Mini-Projects/Calculator App/calculator.py
@@ -1,37 +1,30 @@
-print("Welcome to the Calculator 1.0.")
-print("You can pick any of the choices specified below ")
-print("For Addition:  +")
-print("For Subtraction:  -")
-print("For Multiplication:  *")
-print("For Division:  /")
+print("Welcome to the Calculator 1.0. \n")
+print("You can pick any of the choices specified below \n")
+print("For Addition:  + \n")
+print("For Subtraction:  - \n")
+print("For Multiplication:  * \n")
+print("For Division:  / \n")
+print("")
 while True:
-    a=float(input("Enter the first number : "))
+    a=float(input("Enter the first number : \n"))
+    b=float(input("Enter the second number : \n"))
+    choice=input("Enter your choice : \n")
     print("")
-    b=float(input("Enter the second number : "))
-    print("")
-    choice=input("Enter your choice : ")
     match choice:
         case '+':
-            print("Addition (+) ",a+b)
-            print("")
+            print("Addition (+) ",a+b,"\n")
         case '-':
             if a>b:
-                print("Subtraction (-) ",a-b)
-                print("")
+                print("Subtraction (-) ",a-b,"\n")
             else:
-                print("Subtraction (-) ",b-a)
-                print("")
+                print("Subtraction (-) ",b-a,"\n")
         case '*':
-            print("Multiplication (*) ",a*b)
-            print("")
+            print("Multiplication (*) ",a*b,"\n")
         case '/':
             if a>b:
-                print("Division (/) ",a/b)
-                print("")
+                print("Division (/) ",a/b,"\n")
             else:
-                print("Division (/) ",b/a)
-                print("")
+                print("Division (/) ",b/a,"\n")
         case default:
-            print("Please check the choice you entered.")
-            print("")
+            print("Please check the choice you entered. \n")
             break
