62fd9bec8aa893cf119d3f53dfb636800185c49a
Haider Abbas <haidertgr677@gmail.com>
2024-08-07 23:36:06 +0530

    Leap Year Code Updated

--- HackerRank/Function.py
+++ HackerRank/Function.py
@@ -1,12 +1,14 @@
 import math
+
 def is_leap(year):
     leap = False
-    if year>=1900 and year<=(math.pow(10,5)):
-        if year%4==0:
-            if not(year%100==0) and (year%400==0):
-                leap =True
-    # Write your logic here
-    
+    if year >= 1900 and year <= (math.pow(10, 5)):
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    leap = True
+            else:
+                leap = True
     return leap
 
 year = int(input())
