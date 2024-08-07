386261d62fcd4543174af8b17fc3adda2a6d64a4
Haider Abbas <haidertgr677@gmail.com>
2024-08-07 22:51:16 +0530

    New Hacker Rank Program added

---
+++ HackerRank/Arithmetic-Operations.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    import math
+    a = int(input())
+    b = int(input())
+    if (a>=1 and a<=(math.pow(10,10))) and (b>=1 and b<=(math.pow(10,10))):
+        print(a+b)
+        print(a-b)
+        print(a*b)
